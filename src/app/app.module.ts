37aba75f79992efb4ceb602f07c7dab8965622a2
js <jskierbi@gmail.com>
2016-11-01 23:26:30 +0100

    Finished form tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
 import {KeyupComponent} from "./keyup/keyup.component";
-import { HeroFormComponent } from './hero-form/hero-form.component';
+import {HeroFormComponent} from "./hero-form/hero-form.component";
 
 @NgModule({
     declarations: [
--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -2,21 +2,36 @@ import {Component} from "@angular/core";
 import {HeroDetails} from "../model/HeroDetails";
 
 @Component({
-    selector: 'app-hero-form',
-    templateUrl: './hero-form.component.html',
-    styleUrls: ['./hero-form.component.css']
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
 })
 export class HeroFormComponent {
-    powers = ['Really Smart',
-        'Super flexible',
-        'Super Hot',
-        'Weather Charger'];
 
-    model = new HeroDetails(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  powers = ['Really Smart',
+    'Super flexible',
+    'Super Hot',
+    'Weather Charger'];
 
-    submitted = false;
+  model : HeroDetails;
+  submitted = false;
+  active = true;
 
-    onSubmit() { this.submitted = true; }
+  constructor() {
+    this.newHero();
+  }
 
-    get diagnostic() { return JSON.stringify(this.model); }
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  newHero() {
+    this.model = new HeroDetails(0, '', '');
+    this.active = false;
+    setTimeout(() => this.active = true, 0);
+  }
+
+  get diagnostic() {
+    return JSON.stringify(this.model);
+  }
 }
